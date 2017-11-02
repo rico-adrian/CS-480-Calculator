fc00926fcba8dbcfb7b29ef1ec3da464c40936cb
ricohelvid <33173288+ricohelvid@users.noreply.github.com>
2017-11-02 04:52:08 -0700

    got the whole calculator done with the decimal and many digits. only have to do documentation and exception handling now.

--- WindowsFormsApplication2/Calculator.cs
+++ WindowsFormsApplication2/Calculator.cs
@@ -261,6 +261,7 @@ namespace Calculator
                                 stackOfNumbers.Push(stackOfNumbers.Pop() - result);
                                 break;
                             }
+                       
                     }
                 }
 
@@ -284,7 +285,7 @@ namespace Calculator
         private bool IsDigit(char digit)
         {
              if (digit == '0' || digit == '1' || digit == '2' || digit == '3' || digit == '4' || digit == '5' || digit == '6'
-                || digit == '7' || digit == '8' || digit == '9')
+                || digit == '7' || digit == '8' || digit == '9'||digit=='.')
             {
                 return true;
             }
